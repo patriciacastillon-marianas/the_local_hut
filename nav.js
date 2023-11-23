1aee640c73e0870eb451303405525906ba1c2a10
Patricia Castillon <143171814+patriciacastillon-marianas@users.noreply.github.com>
2023-11-23 22:43:39 +1000

    made mobile navbar

---
+++ nav.js
@@ -0,0 +1,31 @@
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+   
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
+
+// const observer = new IntersectionObserver ((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+
+// const hiddenElements = document.querySelectorAll ('.hidden');
+// hiddenElements.forEach((el) => observer.observe (el));
